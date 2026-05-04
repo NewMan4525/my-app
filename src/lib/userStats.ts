498d3f93df15c1899b020c25754f4307dd73417d
Andrew <newman4525@gmail.com>
2026-05-04 03:47:02 +0300

    add-files

---
+++ src/lib/userStats.ts
@@ -0,0 +1,24 @@
+import { IUserStats } from "@/src/types/interfaces";
+
+export const userStats: IUserStats = {
+  the_forge: {
+    factionStans: 0,
+    stationOwnerStand: 0,
+  },
+  domain: {
+    factionStans: 0,
+    stationOwnerStand: 0,
+  },
+  sinq_laison: {
+    factionStans: 0,
+    stationOwnerStand: 0,
+  },
+  metropolis: {
+    factionStans: 0,
+    stationOwnerStand: 0,
+  },
+  heimatar: {
+    factionStans: 0,
+    stationOwnerStand: 0,
+  },
+};
